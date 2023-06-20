625a94426ff9be0594a756d865485d7b5240bdaf
capstone <igor.mestechkin@gmail.com>
2023-06-20 16:33:23 -0400

    fork check

--- client/App.js
+++ client/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Regulate app! with wire frames ARRRRRRRR!!!</Text>
+      <Text>Regulate app! with wire frames ARRRRRRRR!!! overboardnoreunerovner</Text>
       <StatusBar style="auto" />
     </View>
   );
