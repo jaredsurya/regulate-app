97f6d8d9ab1ce097f9ac4f2980b39831d0099901
capstone <igor.mestechkin@gmail.com>
2023-06-23 03:29:15 -0400

    update App, home, create auth/appscreens

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useState } from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { useFocusEffect } from '@react-navigation/native';
+
+
+import Navbar from './Navbar';
+import Auth from './components/Auth';
 
 export default function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const Stack = createNativeStackNavigator();
 
   const handleSignIn = () => {
     // Perform sign-in logic using the email and password
@@ -13,6 +22,15 @@ export default function Auth() {
     setPassword('')
   };
 
+
+  <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} 
+          options={{ headerTitle: (props) => <Navbar {...props} /> }}
+        />
+        <Stack.Screen name="Auth" component={Auth} options={{ title: 'Overview' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   return (
     <View style={styles.container}>
       <Text>Welcome to REGULATE.</Text>
@@ -37,6 +55,20 @@ export default function Auth() {
   );
 }
 
+  // Tried to use this to autohide status bar
+  // useFocusEffect(useCallback(() => {
+  //   // This will run when screen is `focused` or mounted.
+  //   StatusBar.setHidden(true);
+  
+  //   // This will run when screen is `blured` or unmounted.
+  //   return () => {
+  //     StatusBar.setHidden(false);
+  //   }
+  // }, []));
+const handleButtonPress = () => {
+  // Action to perform when the button is pressed
+  console.log('Button pressed!');
+};
 
 //should we make a dedicated css stylesheet?
 const styles = StyleSheet.create({
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,75 +1,21 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import React, {useState, useCallback} from 'react';
-import { useFocusEffect } from '@react-navigation/native';
-
-import Navbar from './Navbar';
-import Auth from './components/Auth';
+//screen toggle is just for viewing auth or app screens
+//need to implement redux store and loading or home screen while user is being loaded
 
 export default function Home() {
-  const [statusBarToggle, setStatusBarToggle] = useState(true)
-
-  const handleButtonPress = () => {
-    // Action to perform when the button is pressed
-    console.log('Button pressed!');
-  };
-
-  const Stack = createNativeStackNavigator();
-
-  // useFocusEffect(useCallback(() => {
-  //   // This will run when screen is `focused` or mounted.
-  //   StatusBar.setHidden(true);
-  
-  //   // This will run when screen is `blured` or unmounted.
-  //   return () => {
-  //     StatusBar.setHidden(false);
-  //   }
-  // }, []));
+  //set to true for appscreens and false for authscreens
+  const [screenToggle, setScreenToggle] = useState(true);
 
     return (
       <NavigationContainer>
-        {isLoading ? (
-          <Loading_Screen />
-        ) : user ? ( <AppScreens />) : ( <AuthScreens />)}
+        {/* Need to add async for hitting backend, while autlogin is running, display loading screen */}
+          {screenToggle ?
+            <AppScreens /> 
+          : 
+            <AuthScreens />
+          };
       </NavigationContainer>
     );
-  }
-
-<NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} 
-          options={{ headerTitle: (props) => <Navbar {...props} /> }}
-        />
-        <Stack.Screen name="Auth" component={Auth} options={{ title: 'Overview' }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-
-
-  // <View style={styles.container}>
-  //       <Navbar />
-  //   <Text>This is the APP component.</Text>
-  //   <Text>Blah Blah Blah Blah Blah</Text>
-  //   <Button title="Random button!" onPress={handleButtonPress} />
-  //   <StatusBar style="light" />
-  // </View>
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  };
